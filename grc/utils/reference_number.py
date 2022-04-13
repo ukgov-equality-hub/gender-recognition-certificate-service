02dbdd5c311bf9f57b9b3184588bb9f73196c12f
James Griffiths <james@jwgsoftware.com>
2022-04-13 14:52:38 +0100

    259: Improvement: allow user to type spaces in reference number

--- grc/utils/reference_number.py
+++ grc/utils/reference_number.py
@@ -20,13 +20,13 @@ def reference_number_generator(email):
 
 
 def reference_number_string(reference_number):
-    trimmed_reference = reference_number.replace('-', '').upper()
+    trimmed_reference = reference_number.replace('-', '').replace(' ', '').upper()
     formatted_reference = trimmed_reference[0:4] + '-' + trimmed_reference[4: 8]
     return formatted_reference
 
 
 def validate_reference_number(reference):
-    reference = reference.replace('-', '').upper()
+    reference = reference.replace('-', '').replace(' ', '').upper()
     record = Application.query.filter_by(reference_number=reference).first()
 
     if record is None:
