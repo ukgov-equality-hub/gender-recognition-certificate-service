5d5d46c6cf6f6dd99b29963f2f962e4a02422518
alistairknight <alistair@nts-graphics.co.uk>
2022-04-25 16:32:17 +0100

    Pytest e2e

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,6 +1,5 @@
 
-
-
 def test_example_is_working(page):
     page.goto("http://localhost:8080")
-    assert page.inner_text('a.govuk-header__link') == 'Apply for a Gender Recognition Certificate'
+    assert page.inner_text('a.govuk-header__link.govuk-header__link--service-name') == 'Apply for a Gender Recognition Certificate'
+
