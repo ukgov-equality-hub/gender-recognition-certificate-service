5bd1df3b64c5ae107302b1e8c57c3b5c15c9f954
James Griffiths <james@jwgsoftware.com>
2022-05-05 11:47:51 +0100

    321: Database: Get connection string from Cloud Foundry VCAP_SERVICES environment variable (if present)

--- grc/config.py
+++ grc/config.py
@@ -1,12 +1,21 @@
 import logging
 import os
 from os.path import dirname
+from grc.utils.config_helper import ConfigHelper
+
+
+def get_connection_string() -> str:
+    if ConfigHelper.get_vcap_services() is not None:
+        creds = ConfigHelper.get_vcap_services().postgres[0].credentials
+        return f"postgresql://{creds.username}:{creds.password}@{creds.host}:{creds.port}/{creds.database_name}"
+    else:
+        return os.environ.get("SQLALCHEMY_DATABASE_URI")
 
 
 class Config:
     BASE_DIRECTORY = dirname(dirname(os.path.abspath(__file__)))
     ENVIRONMENT = os.environ.get("ENVIRONMENT", "PRODUCTION")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = get_connection_string()
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEFAULT_ADMIN_USER = os.environ.get("DEFAULT_ADMIN_USER")
     LOG_LEVEL = (
