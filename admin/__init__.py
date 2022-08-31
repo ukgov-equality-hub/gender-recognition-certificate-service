d9e4a1b080007920c184f3d4323b525c58b3128b
James Griffiths <james@jwgsoftware.com>
2022-08-31 13:50:12 +0100

    3912: Remove unnecessary and sometimes-confusing add_url_rule() commands

--- admin/__init__.py
+++ admin/__init__.py
@@ -71,37 +71,31 @@ def create_app(test_config=None):
     # Admin page
     from admin.admin import admin
     app.register_blueprint(admin)
-    app.add_url_rule('/', endpoint='index')
 
     # Signout
     from admin.signout import signout
     app.register_blueprint(signout)
-    app.add_url_rule('/signout', endpoint='index')
 
     # Password reset
     from admin.password_reset import password_reset
     app.register_blueprint(password_reset)
-    app.add_url_rule('/password_reset', endpoint='index')
 
     # Forgot password
     from admin.forgot_password import forgot_password
     app.register_blueprint(forgot_password)
-    app.add_url_rule('/forgot_password', endpoint='index')
 
     # Applications
     from admin.applications import applications
     app.register_blueprint(applications)
-    app.add_url_rule('/applications', endpoint='index')
 
     # Manage users
     from admin.users import users
     app.register_blueprint(users)
-    app.add_url_rule('/users', endpoint='index')
 
     # System jobs
     from admin.jobs import jobs
     app.register_blueprint(jobs)
-    app.add_url_rule('/jobs', endpoint='index')
+
 
     # Health Check
     from admin.health_check import health_check
