d7818ff7de11eb18bc1842ed794bc584d4c1462b
James Griffiths <james@jwgsoftware.com>
2022-09-07 15:35:52 +0100

    3912: Admin PDF tool: Re-use file upload validation from public-facing site

--- admin/tools/forms.py
+++ admin/tools/forms.py
@@ -1,12 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import FileField, PasswordField
+from wtforms import PasswordField, MultipleFileField
 from wtforms.validators import DataRequired
+from grc.utils.form_custom_validators import MultiFileAllowed, fileSizeLimit, fileVirusScan
 
 
 class UnlockFileForm(FlaskForm):
-    file = FileField(
+    file = MultipleFileField(
         validators=[
-            DataRequired(message='Select a file to upload')
+            DataRequired(message='Select a PDF file to upload'),
+            MultiFileAllowed(['pdf'], message='Select a PDF file to upload'),
+            fileSizeLimit(10),
+            fileVirusScan
         ]
     )
 
