0f307818779f31ebe03f3fc1ae34a3d0a6a8b7b7
Mark Butler <mark.butler@solirius.com>
2023-02-09 13:13:24 +0000

    Re-introduce flask commands

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "In run.sh...." 1>&2
+echo "In run.sh...."
 
 export FLASK_APP=grc
-#flask db init
-#flask db migrate
-#flask db upgrade
+flask db init
+flask db migrate
+flask db upgrade
 
-#flask run --host=0.0.0.0
+flask run --host=0.0.0.0
 
-while true
-do
-  sleep 30
-done
+#while true
+#do
+#  sleep 30
+#done
