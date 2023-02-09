39920b9dbcf29d15b7ddf9cb8ac312ee6f50a4e4
Mark Butler <mark.butler@solirius.com>
2023-02-09 10:44:11 +0000

    Run a dummy CMD at startup

--- run.sh
+++ run.sh
@@ -5,8 +5,13 @@ echo "In run.sh...." 1>&2
 find . -name "*.py" 1>&2
 
 export FLASK_APP=grc
-flask db init
-flask db migrate
-flask db upgrade
+#flask db init
+#flask db migrate
+#flask db upgrade
 
-flask run --host=0.0.0.0
+#flask run --host=0.0.0.0
+
+while true
+do
+  sleep 30
+done
