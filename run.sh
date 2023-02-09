5df59350ab706bc9859ad73d0042e364e235a29d
Mark Butler <mark.butler@solirius.com>
2023-02-09 18:00:17 +0000

    Add in FLASK_ENV

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "DATABASE_URL is <${DATABASE_URL}>"
-echo "FLASK_APP is >${FLASK_APP}>"
+echo "FLASK_APP is <${FLASK_APP}>"
+echo "FLASK_ENV is <${FLASK_ENV}>"
 
+echo "flask db commands commented out to bypass db migration"
 #flask db init
 #flask db migrate
 #flask db upgrade
