442a08987f0f170853d9e25a92dc693e06e84e9e
Mark Butler <mark.butler@solirius.com>
2023-02-09 17:48:40 +0000

    Comment out the flask db commands to see if the application will start

--- run.sh
+++ run.sh
@@ -3,11 +3,8 @@
 echo "DATABASE_URL is <${DATABASE_URL}>"
 echo "FLASK_APP is >${FLASK_APP}>"
 
-export FLASK_ENV=production
-export FLASK_DEBUG=1
-
-flask db init
-flask db migrate
-flask db upgrade
+#flask db init
+#flask db migrate
+#flask db upgrade
 
 flask run --host=0.0.0.0
