8b3857a99add97830a34c39f83714c429f5ea2d0
Mark Butler <mark.butler@solirius.com>
2023-02-27 09:30:09 +0000

    Use waitress-serve for all 3 app start-ups Change Gov Pay live service key Test shutter of main app

--- run.sh
+++ run.sh
@@ -8,5 +8,6 @@ echo "flask db commands commented out to bypass db migration"
 #flask db init
 #flask db migrate
 #flask db upgrade
+#flask run --host=0.0.0.0 -p 3000
 
-flask run --host=0.0.0.0 -p 3000
+waitress-serve --call --port=3000 'grc:create_app'
--- run_admin.sh
+++ run_admin.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-flask run --host=0.0.0.0 --port=3001
+#flask run --host=0.0.0.0 --port=3001
+waitress-serve --call --port=3001 'admin:create_app'
