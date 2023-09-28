77327e4b65d9c2a3f3c18311fa873346edcd5749
itoulou <touloumbadjiani@gmail.com>
2023-09-28 16:00:48 +0100

    add strategy

--- grc/utils/limiter.py
+++ grc/utils/limiter.py
@@ -8,12 +8,13 @@ def limiter(app):
     print(f'MEMORY_STORAGE_URL APP = {app.config["MEMORY_STORAGE_URL"]}')
     if not app.config['MEMORY_STORAGE_URL']:
         return None
-    storage_uri = app.config['MEMORY_STORAGE_URL'].replace('rediss://', 'redis+cluster://dummy_user')
-    print(f'storage_uri = {storage_uri}', flush=True)
+    # storage_uri = app.config['MEMORY_STORAGE_URL'].replace('rediss://', 'redis+cluster://dummy_user')
+    # print(f'storage_uri = {storage_uri}', flush=True)
     return Limiter(
         get_remote_address,
         app=app,
         meta_limits=["5 per minute"],
         default_limits=["200 per day", "50 per hour"],
-        storage_uri=storage_uri
+        storage_uri=app.config['MEMORY_STORAGE_URL'],
+        strategy="fixed-window"
     )
